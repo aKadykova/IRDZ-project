1567d4a12799737254b48dcae80199abaa992d44
Anastasiia Kadykova <a.a.kadykova@gmail.com>
2026-09-10 23:03:53 +0200

    upgraded project routes

--- server/src/api/project/routes/project.ts
+++ server/src/api/project/routes/project.ts
@@ -7,7 +7,7 @@ import { factories } from '@strapi/strapi';
 export default factories.createCoreRouter('api::project.project', {
 	config: {
 		find: {
-			middlewares: ['api::project.project-response'],
+			middlewares: ['global::content-response'],
 		},
 		findOne: {
 			middlewares: ['api::project.project-response'],
